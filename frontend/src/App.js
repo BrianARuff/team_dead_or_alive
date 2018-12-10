ee1a1e9cd1333d17c25b68caa4577a553d5c4d2b
jheinz1999 <jonathan.heinz@aol.com>
2018-12-10 08:40:12 -0800

    login start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     super();
     this.state = {
 
-      serverResp: 'waiting for response...'
+
 
     }
 
@@ -25,7 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>{this.state.serverResp}</p>
+        <p>Testing</p>
       </div>
     );
   }
