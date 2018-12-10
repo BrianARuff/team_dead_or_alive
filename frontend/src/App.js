0fceee80e8fa224ef4607380b6ee5a37abef8135
jheinz1999 <jonathan.heinz@aol.com>
2018-12-10 13:52:57 -0800

    testing without login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        {!this.props.username ? <Login /> : <BrowserRouter><Router /></BrowserRouter>}
+        {this.props.username ? <Login /> : <BrowserRouter><Router /></BrowserRouter>}
       </div>
     );
   }
