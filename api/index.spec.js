6fa9b47d7bb80a3004a6dda78621d9891794de18
Chad Jemmett <chad.jemmett@gmail.com>
2018-12-10 10:02:10 -0700

    Added dev packages.

---
+++ api/index.spec.js
@@ -0,0 +1,12 @@
+const request = require('supertest')
+
+const server = require('./index.js')
+
+describe('server', () => {
+  describe('/ route', () => {
+    it('should return status code 200', async () => {
+      let response = await request(server).get('/')
+      expect(response.status).toBe(200)
+    })
+  })
+})
