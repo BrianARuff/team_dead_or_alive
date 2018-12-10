d926842a58bf7c69df94ee90f3773bd438d784cd
jheinz1999 <jonathan.heinz@aol.com>
2018-12-10 08:55:48 -0800

    set up redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(() => {}, applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
