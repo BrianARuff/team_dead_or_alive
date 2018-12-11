982ace75399a691e90ca9712669be037b1d99308
Michael Landers <landers.mike@gmail.com>
2018-12-11 08:42:38 -0800

    * created hamburger menu for mobile * header is smaller in mobile * header is stacked in mobile * added fade in to header elements on desktop

--- marketing/script.js
+++ marketing/script.js
@@ -35,9 +35,15 @@ let creditContent = document.querySelector('.footerContent')
 $(credits).click( () => $(creditContent).slideToggle( "slow"));
 
 //login modal
-let login = document.querySelector('.login')
+const login = document.querySelector('.login')
+const modalLogin = document.querySelector('.mLogin')
 
 login.addEventListener('click', () => {
+    document.getElementById('id01').style.display = 'block'
+})
+modalLogin.addEventListener('click', () => {
+    let modal = document.querySelector('.menuModal');
+    modal.style.display = "none";
     document.getElementById('id01').style.display = 'block'
 })
 
@@ -48,3 +54,34 @@ window.addEventListener('click', event => {
         modal.style.display = "none";
     }})
 
+//Fade in of header items
+const titleL = document.querySelector('.titleLeft');
+const dead = document.querySelector('.headerLeft img');
+const o = document.querySelector('.or');
+const titleR = document.querySelector('.titleRight');
+const alive = document.querySelector('.headerRight img');
+const signUp = document.querySelector('.signUp')
+let w = window.innerWidth;
+console.log(w);
+
+if(w >= 500){
+    $(window).ready(() => {
+        $(titleL).hide().fadeIn(2000);
+        $(dead).hide().fadeIn(2000);
+        $(o).hide().delay(1000).fadeIn(2000);
+        $(titleR).hide().delay(2000).fadeIn(2000);
+        $(alive).hide().delay(2000).fadeIn(2000);
+        $(signUp).hide().delay(4000).fadeIn(2000);
+      });
+}
+
+//Mobile Menu
+const hamburger = document.querySelector('.fa-bars');
+const menuContent = document.querySelector('.menuModal')
+hamburger.addEventListener('click', ()=> menuContent.style.display = "block")
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', event => {
+    let modal = document.querySelector('.menuModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }})
