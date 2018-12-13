08dd1e007bab0a4c7271f21cd565d6783eacb681
Mike Landers <landers.mike@gmail.com>
2018-12-13 09:49:28 -0800

    * design update to use cards instead of a list

--- frontend/src/components/QuizPreview.js
+++ frontend/src/components/QuizPreview.js
@@ -7,11 +7,11 @@ export default function QuizPreview({quizData, history}) {
   return (
 
     <div className='quiz-preview' onClick={() => history.push(`/game/${quizData.id}`)}>
-
+    
       <h2>{quizData.name}</h2>
       <div className='details'>
-        <p className='author'>By <span className='user-link' onClick={e => { e.stopPropagation(); history.push(`/users/${quizData.user_id}`) }}>Joe Schmoe</span></p>
-        <p>Number of questions: 42</p>
+        <p className='author'>Author <span className='user-link' onClick={e => { e.stopPropagation(); history.push(`/users/${quizData.user_id}`) }}><br/>Joe Schmoe</span></p>
+        <p>42<br></br>Questions</p>
       </div>
 
     </div>
